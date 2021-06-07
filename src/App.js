08f25677fc1b91c8f840e9986c6897383c121684
Shubham Raj <SINGH29199810@GMAIL.COM>
2021-06-07 13:36:07 +0530

    Nav Bar Updated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { Vehicle } from "./component/Vehicle";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AppNavBar } from "./common/AppNavBar";
 // vehicle
 function App() {
-  return <Vehicle></Vehicle>;
+  return (
+    <Router>
+      <AppNavBar />
+    </Router>
+  );
 }
 
 export default App;
