921c99a7ef5a78690663bf2f47df485e41c53809
reactvehiclerentalsystem <84133001+reactvehiclerentalsystem@users.noreply.github.com>
2021-06-08 17:52:14 +0530

    admin login w redux

--- src/component/AdminLogin.js
+++ src/component/AdminLogin.js
@@ -1,6 +1,44 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
 import { AppNavBar } from "../common/AppNavBar";
+import { AdminLoginAction } from "../redux/AdminLoginReducer";
 
 export function AdminLogin() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const state = useSelector((state) => state);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [successOperation, setSuccessOperation] = useState(false);
+  const [errorOperation, setErrorOperation] = useState(false);
+
+  const updateEmail = (e) => setEmail(e.target.value);
+  const updatePassword = (e) => setPassword(e.target.value);
+
+  const AdminLoginFunction = () => {
+    dispatch(
+      AdminLoginAction({
+        email,
+        password,
+      })
+    );
+
+    setErrorOperation(true);
+    setTimeout(() => setErrorOperation(false), 5000);
+
+    console.log(email, password);
+    console.log(state.email, state.password);
+  };
+
+  if (state.AdminLogin.loginAction === true) {
+    history.push("/");
+
+    return <div></div>;
+  }
+
   return (
     <div>
       <AppNavBar />
@@ -8,22 +46,30 @@ export function AdminLogin() {
         <div className="col-3 col-md-3 d-none d-md-block"></div>
         <div className="col-12 col-md-6">
           <h3 className="alert alert-secondary">Admin Login</h3>
+
+          {state.UserLogin.loginAction === false && errorOperation && (
+            <div className="alert alert-danger">login failure</div>
+          )}
+
           <input
             type="text"
             className="form-control mb-2"
             placeholder="Enter Email"
             required="required"
+            onChange={(e) => updateEmail(e)}
           />
           <input
             type="password"
             className="form-control mb-2"
             placeholder="Enter Password"
             required="required"
+            onChange={(e) => updatePassword(e)}
           />
           <input
             type="button"
             className="btn btn-outline-dark w-100"
             value="Login"
+            onClick={() => AdminLoginFunction()}
           />
         </div>
         <div className="col-3 col-md-3  d-none d-md-block"></div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,8 @@ import { combineReducers } from "redux";
 import { applyMiddleware } from "redux";
 import { createStore } from "redux";
 import thunk from "redux-thunk";
+import { AdminLoginReducer } from "./AdminLoginReducer";
 import { UserLoginReducer } from "./UserLoginReducer";
-
 import { UserRegisterReducer } from "./UserRegisterReducer";
 import { VehicleBrandReducer } from "./VehicleBrandReducer";
 
@@ -11,6 +11,7 @@ const rootReducer = combineReducers({
   UserRegister: UserRegisterReducer,
   UserLogin: UserLoginReducer,
   VehicleBrandR: VehicleBrandReducer,
+  AdminLogin: AdminLoginReducer,
   // depart : DepartRducer,
   //vechil: VechuileReucer
 });
