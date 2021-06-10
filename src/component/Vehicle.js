f331cf05136d01b62853c0c3e6ca9126cd33548e
Shubham Raj <SINGH29199810@GMAIL.COM>
2021-06-10 18:36:58 +0530

    Validation done for vehicle part

--- src/component/Vehicle.js
+++ src/component/Vehicle.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AdminNavBar } from "../common/AppNavBar";
 import { getAllBrandAction } from "../redux/VehicleBrandReducer";
@@ -7,6 +7,7 @@ import { updateVehicleAction, VehicleAction } from "./../redux/VehicleReducer";
 export function Vehicle() {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
+  const formEL = useRef();
 
   const [vehicleName, setVehicleName] = useState(
     state.Vehiclee.refvehicle.vehicleName
@@ -55,34 +56,42 @@ export function Vehicle() {
 
   const AddVehicle = (e) => {
     e.preventDefault();
-    dispatch(
-      VehicleAction({
-        vehicleName,
-        vehicleType,
-        vehiclePlateNumber,
-        vehicleColor,
-        vehicleLocation,
-        numberOfSeats,
-        dailyPrice,
-        picture,
-        vehicleBrand: {
-          brand_id: vehicleBrand,
-        },
-      })
-    );
 
-    setSuccessOperation(true);
-    setTimeout(() => setSuccessOperation(false), 2000);
+    if (formEL.current.checkValidity() === false) {
+      // hanlde the false case
+      e.preventDefault();
+      e.stopPropagation();
+      formEL.current.classList.add("was-validated");
+    } else {
+      dispatch(
+        VehicleAction({
+          vehicleName,
+          vehicleType,
+          vehiclePlateNumber,
+          vehicleColor,
+          vehicleLocation,
+          numberOfSeats,
+          dailyPrice,
+          picture,
+          vehicleBrand: {
+            brand_id: vehicleBrand,
+          },
+        })
+      );
 
-    setVehicleName("");
-    setVehicleType("");
-    setVehiclePlateNumber("");
-    setVehicleColor("");
-    setVehicleLocation("");
-    setNumberOfSeats("");
-    setDailyPrice("");
-    setPicture("");
-    setVehicleBrand("");
+      setSuccessOperation(true);
+      setTimeout(() => setSuccessOperation(false), 2000);
+
+      setVehicleName("");
+      setVehicleType("");
+      setVehiclePlateNumber("");
+      setVehicleColor("");
+      setVehicleLocation("");
+      setNumberOfSeats("");
+      setDailyPrice("");
+      setPicture("");
+      setVehicleBrand("");
+    }
   };
 
   const updateVehicleDetails = (e) => {
@@ -134,98 +143,107 @@ export function Vehicle() {
             <div className="alert alert-success">Vehicle Added</div>
           )}
 
-          <input
-            type="text"
-            className="form-control mb-2"
-            placeholder="Vehicle Name"
-            value={vehicleName}
-            onChange={(e) => updatevehicleName(e)}
-            required
-          />
-          <input
-            type="text"
-            className="form-control mb-2"
-            placeholder="Vehicle Type"
-            value={vehicleType}
-            onChange={(e) => updatevehicleType(e)}
-            required
-          />
-          <select
-            className="form-control form-select w-100 p-2 mb-2"
-            aria-label=".form-select-lg example"
-            value={vehicleBrand}
-            onChange={(e) => updatevehicleBrand(e)}
-          >
-            <option selected>Brand Name</option>
-            {[...state.VehicleBrandR.list].map((item, index) => (
-              <option value={item.brand_id} key={index}>
-                {item.brand_name}
-              </option>
-            ))}
-          </select>
-          <input
-            type="text"
-            className="form-control mb-2"
-            placeholder="Vehicle Plate Number"
-            value={vehiclePlateNumber}
-            onChange={(e) => updatevehiclePlateNumber(e)}
-            required
-          />
-          <input
-            type="text"
-            className="form-control mb-2"
-            placeholder="Vehicle Color"
-            value={vehicleColor}
-            onChange={(e) => updatevehicleColor(e)}
-            required
-          />
-          <input
-            type="text"
-            className="form-control mb-2"
-            placeholder="Number of Seats"
-            value={numberOfSeats}
-            onChange={(e) => updatenumberOfSeats(e)}
-            required
-          />
-          <input
-            type="text"
-            className="form-control mb-2"
-            placeholder="Daily Price"
-            value={dailyPrice}
-            onChange={(e) => updatedailyPrice(e)}
-            required
-          />
-          <input
-            type="text"
-            className="form-control mb-2"
-            placeholder="Vehicle Location"
-            value={vehicleLocation}
-            onChange={(e) => updatevehicleLocation(e)}
-            required
-          />
-          <input
-            type="text"
-            className="form-control mb-2"
-            placeholder="Vehicle Picture Path"
-            value={picture}
-            onChange={(e) => updatepicture(e)}
-            required
-          />
-          {state.Vehiclee.refvehicle.vehicleId ? (
+          <form ref={formEL} class="needs-validation" novalidate>
             <input
-              type="button"
-              className="btn btn-outline-success w-100"
-              value="Update Vehicle"
-              onClick={(e) => updateVehicleDetails(e)}
+              type="text"
+              className="form-control mb-2"
+              placeholder="Vehicle Name"
+              value={vehicleName}
+              onChange={(e) => updatevehicleName(e)}
+              pattern="^[A-Z].*"
+              required
             />
-          ) : (
             <input
-              type="button"
-              className="btn btn-outline-success w-100"
-              value="Add Vehicle"
-              onClick={(e) => AddVehicle(e)}
+              type="text"
+              className="form-control mb-2"
+              placeholder="Vehicle Type"
+              value={vehicleType}
+              onChange={(e) => updatevehicleType(e)}
+              pattern="^[A-Z].*"
+              required
             />
-          )}
+            <select
+              className="form-control form-select w-100 p-2 mb-2"
+              aria-label=".form-select-lg example"
+              value={vehicleBrand}
+              onChange={(e) => updatevehicleBrand(e)}
+            >
+              <option selected>Brand Name</option>
+              {[...state.VehicleBrandR.list].map((item, index) => (
+                <option value={item.brand_id} key={index}>
+                  {item.brand_name}
+                </option>
+              ))}
+            </select>
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Vehicle Plate Number     Ex: BR-23-CD-2340"
+              value={vehiclePlateNumber}
+              onChange={(e) => updatevehiclePlateNumber(e)}
+              pattern="^[A-Z]{2}[-][0-9]{2}[-][A-Z]{2}[-][0-9]{4}$"
+              required
+            />
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Vehicle Color"
+              value={vehicleColor}
+              onChange={(e) => updatevehicleColor(e)}
+              pattern="^[A-Z].*"
+              required
+            />
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Number of Seats"
+              value={numberOfSeats}
+              onChange={(e) => updatenumberOfSeats(e)}
+              pattern="\d"
+              required
+            />
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Daily Price"
+              value={dailyPrice}
+              onChange={(e) => updatedailyPrice(e)}
+              pattern="^[0-9]*$"
+              required
+            />
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Vehicle Location"
+              value={vehicleLocation}
+              onChange={(e) => updatevehicleLocation(e)}
+              pattern="^[A-Z].*"
+              required
+            />
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Vehicle Picture Path"
+              value={picture}
+              onChange={(e) => updatepicture(e)}
+              required
+            />
+            {state.Vehiclee.refvehicle.vehicleId ? (
+              <input
+                type="button"
+                className="btn btn-outline-success w-100"
+                value="Update Vehicle"
+                onClick={(e) => updateVehicleDetails(e)}
+              />
+            ) : (
+              <input
+                type="button"
+                className="btn btn-outline-success w-100"
+                value="Add Vehicle"
+                onClick={(e) => AddVehicle(e)}
+              />
+            )}
+          </form>
         </div>
         <div className="col-3 col-md-3  d-none d-md-block"></div>
       </div>
